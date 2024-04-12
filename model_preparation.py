ac8e9f414e1affe0fd4b1efdb443ca17adaca47a
Danila Moiseev <140072030+AnalystSurfer@users.noreply.github.com>
2024-04-12 16:15:08 +0300

    Обучаем модель

--- model_preparation.py
+++ model_preparation.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.svm import SVR
 import joblib
 
+
 preprocessed_train_data = pd.read_csv('train/preprocessed_train_data.csv')
 
 model = SVR(kernel='rbf', C=100, gamma=0.1)
