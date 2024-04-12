d997a615280baca525476649dc21e18b9c913773
Danila Moiseev <140072030+AnalystSurfer@users.noreply.github.com>
2024-04-12 16:15:56 +0300

    Предобработка данных

--- model_preprocessing.py
+++ model_preprocessing.py
@@ -6,13 +6,11 @@ import numpy as np
 train_data = pd.read_csv('train/train_data.csv')
 test_data = pd.read_csv('test/test_data.csv')
 
-
 scaler = StandardScaler()
 X_train = scaler.fit_transform(train_data[['x']])
 y_train = train_data['y'].to_numpy()
 X_test = scaler.transform(test_data[['x']])
 y_test = test_data['y'].to_numpy()
-
 
 preprocessed_train_data = pd.DataFrame(np.hstack((X_train, y_train.reshape(-1, 1))), columns=['x', 'y'])
 preprocessed_test_data = pd.DataFrame(np.hstack((X_test, y_test.reshape(-1, 1))), columns=['x', 'y'])
